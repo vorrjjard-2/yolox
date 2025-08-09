dfd8940b3ba402c76a81943cb081b543fdccba0c
vorrjjard <ruben.saulog@gmail.com>
2025-08-09 21:43:53 +0800

    fixed some shit bruh

--- dataset.py
+++ dataset.py
@@ -4,21 +4,35 @@ import torch
 from PIL import Image, ImageFile
 from torch.utils.data import Dataset, DataLoader
 
+from PIL import Image, ImageFile
+
+from utils import (
+    pre_index
+)
+
+import json
+import os
+
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 class YOLODataset(Dataset):
     def __init__(self, 
                  split, 
-                 annotations,
                  S=[13, 26, 52],
                  transform=None
                    ):
         super().__init__()
         self.split = split
-        self.annotations = annotations
         self.S = S
         self.transform = transform
-
+        self.annotations = pre_index(os.path.join('datasets', config.DATASET, self.split, '_annotations.coco.json'))
 
     def __len__(self):
         return len(self.annotations)
+    
+    def __getitem__(self, index):
+        image = np.array(Image.open(img_path).convert("RGB"))
+            
+
+
+        return super().__getitem__(index)
--- utils.py
+++ utils.py
@@ -7,12 +7,28 @@ from collections import defaultdict
 def pre_index(annotation_path):
     with open(annotation_path, 'r') as file:
         annotations = json.load(file)
-    data = defaultdict(list)
+
+    id_annotations = defaultdict(list)
+    id_images = {}
+    id_categories = {} 
+
+    #ID TO ANNOTATIONS
 
     for item in annotations["annotations"]:
         img_id = item['image_id']
-        data[img_id].append([item['category_id']] + item['bbox'])
+        id_annotations[img_id].append([item['category_id']] + item['bbox'])
 
-    return data
+    # ID TO IMAGE
+    for item in annotations["images"]:
+        id = item['id']
+        id_images[id] = [item['file_name'], item['width'], item['height']]
+    
+    for item in annotations['categories']:
+        category_id
+        
 
 
+    return id_annotations
+
+data = pre_index('datasets/COCO/train/_annotations.coco.json')
+print(data[3])
